53f4f53a5cedff492a9328983b72606949c6291b
Eusébio Alves <eusebio.a95@hotmail.com>
2023-08-20 11:26:18 +0100

    Adding dynamic blur on mouse over

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log('its alive!!');
+const backgroundImage = document.querySelector('.bg-image');
+const rightPannel = document.querySelector('.right-pannel');
+
+
+
+rightPannel.addEventListener('mouseenter', () => addBlur(backgroundImage));
+
+rightPannel.addEventListener('mouseleave', () => removeBlur(backgroundImage));
+
+
+function addBlur(image) {
+    image.style.cssText = 'filter: blur(8px)';
+}
+
+function removeBlur(image) {
+    image.style.cssText = '';
+}
+
+
+//backgroundImage.style.cssText = 'filter: blur(8px);';
